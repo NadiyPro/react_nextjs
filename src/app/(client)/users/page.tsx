1a7217e564948f55cb6cd8778fa14ab0c1e8ec84
NadiyPro <siroviyn13@gmail.com>
2024-08-09 18:54:57 +0300

    create

---
+++ src/app/(client)/users/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from "next/link";
+import {getAllUsers} from "@/app/services/api.service";
+
+const UsersPage = async () => {
+
+    let allUsers = await getAllUsers();
+
+    return (
+        <div>
+            <ul>
+
+                {
+                    allUsers.map(value => <li key={value.id}>
+                        <Link href={{pathname: '/users/' + value.id, query: {data: JSON.stringify(value)}}}>{value.username}</Link>
+                    </li>)
+
+
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default UsersPage;
