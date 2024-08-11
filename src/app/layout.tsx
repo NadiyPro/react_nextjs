8f0f319fa1240dcfb70778de98a14005341b29fc
NadiyPro <siroviyn13@gmail.com>
2024-08-11 14:04:54 +0300

    create

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,8 @@ export default function RootLayout({
   return (
       <html lang="en">
       <body className={inter.className}>
-
       <HeaderComponent/>
       {children}
-
-
       </body>
       </html>
   );
